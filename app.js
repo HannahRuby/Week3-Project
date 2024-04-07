c7c4cc3bd90dc382ee2f6076da7859bf6571e4c0
HANNAH <HUPEE2001@YAHOO.COM>
2024-04-07 05:47:15 +0100

    added accessibilty features

--- app.js
+++ app.js
@@ -8,43 +8,41 @@ const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 const text = document.getElementById("text");
 
-elements.forEach(function (element) {
-  element.addEventListener("focus", (e) => {
-    console.log(`Element ${e.currentTarget} is focused`);
-  });
-});
-
 let imageIndex = 0;
 
 const images = [
   {
-    url: "https://images.unsplash.com/photo-1594679683518-7d61030ed39c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "./assets/luis-arias-1mNAVJaPr-g-unsplash.jpg",
     alt: "2-children-playing-on-green-and-blue-plastic-slide",
   },
 
   {
-    url: "https://images.unsplash.com/photo-1561567131-f7d83083aee0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "baby-wearing-blue-blanket",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1610901137736-d7cc46657b11?q=80&w=2035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "baby-lying-on-blue-textile",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1566004100631-35d015d6a491?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "baby-under-purple-blanket",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1630305130592-210da48f151e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "baby-in-white-bath-towel",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1552788960-65fcafe071a5?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "./assets/christian-bowen-uFCkcE6GI40-unsplash.jpg",
     alt: "smiling-baby-lying-forward-on-pink-textile",
   },
   {
-    url: "https://images.unsplash.com/photo-1515488042361-ee00e0ddd4e4?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "./assets/lubomirkin-XKpPsuuGE_Q-unsplash.jpg",
+    alt: "baby-throwing-water-in-the-air",
+  },
+  {
+    url: "./assets/senjuti-kundu-JfolIjRnveY-unsplash.jpg",
+    alt: "girl covered in paint",
+  },
+  {
+    url: "./assets/nihal-karkala-M5aSbOXeDyo-unsplash.jpg",
+    alt: "baby-in-white-bath-towel",
+  },
+  {
+    url: "./assets/juan-encalada-7E-hgv51mqI-unsplash.jpg",
+    alt: "smiling-baby-standing-on-a-sofa",
+  },
+  {
+    url: "./assets/yuri-shirota-p0hDztR46cw-unsplash.jpg",
     alt: "boy-sitting-on-white-cloth-surrounded-by-toys",
+  },
+  {
+    url: "./assets/zoe-graham-mjrif-KW5zw-unsplash.jpg",
+    alt: "baby sleeping",
   },
 ];
 
@@ -89,3 +87,9 @@ function showNextImage() {
   imageIndex = imageIndex += 1;
   createBigImage(images[imageIndex]);
 }
+
+// elements.forEach(function (element) {
+//   element.addEventListener("focus", (e) => {
+//     console.log(`Element ${e.currentTarget} is focused`);
+//   });
+// });
